37049b507d461c2b3983a5f88323dbb55e519cfe
bbomm1006 <bbomm1006@gmail.com>
2026-03-20 11:28:16 +0900

    챗봇

--- lib/__tail.php
+++ lib/__tail.php
@@ -1,6 +1,8 @@
 <button id="backToTop" title="위로 가기"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 15L12 9L6 15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
 <script src="js/top.js"></script>
 
+<script src="chatbot/chatbot.js"></script>
+
     <?php if (!empty($script['body_code'])) echo $script['body_code']; ?>
     
 </body>
