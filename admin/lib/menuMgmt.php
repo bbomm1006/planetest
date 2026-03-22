91891d6cc9878d5fb9dd85839ec72db5858bebae
bbomm1006 <bbomm1006@gmail.com>
2026-03-22 15:54:04 +0900

    관리자 기능 관리 여백 수정

--- admin/lib/menuMgmt.php
+++ admin/lib/menuMgmt.php
@@ -1,6 +1,6 @@
 <!-- 0-2. 관리자 기능 관리 -->
-      <div class="page" id="page-menuMgmt" style="margin:0">
-        <div class="page-header">
+      <div class="page" id="page-menuMgmt">
+        <div class="page-header" style="margin:0">
         <div><h2>관리자 기능 관리</h2><p>관리자 사이드바 메뉴 노출 여부를 설정합니다.</p></div>
           <button class="btn btn-primary" onclick="saveMenuMgmt()">💾 저장</button>
         </div>
